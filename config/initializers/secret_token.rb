24436a23d8de5406f947b38fd02659d171859253
Ramy <ramy.abdelazim@gmail.com>
2013-11-11 20:49:48 -0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Babysitter::Application.config.secret_key_base = '348efb595a5df963aa8b81b433e8eb20fe794ebddbf2d285b6c296b0651aa3ec0fbedcb063c3a121634e43071d755794ab88d91241f7680e8593ce1215859f09'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Babysitter::Application.config.session_store :cookie_store, key: '_babysitter_session'
